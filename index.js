8f6d218a763771b05c7c5e8209602ac137dab758
Ninian Bond <ninrhythm@gmail.com>
2025-12-19 10:37:10 -0500

    perfected solution for remove_min

--- index.js
+++ index.js
@@ -1,20 +1,4 @@
 function removeSmallest(numbers) {
-  let lowest = 0 
-  
-  for (let i = 0; i < numbers.length; i++) {
-    if (i === 0) {
-      lowest = numbers[i]
-    }
-    if ( lowest > numbers[i]) {
-      lowest = numbers[i]
-    }
-  }
-  console.log(lowest)
-  for ( let i = 0 ; i < numbers.length; i++ ) {
-    if ( numbers[i] === lowest) {
-      numbers.splice(i, 1)
-      return numbers
-    }
-  }
-  return [];
+  let indexOfMin = numbers.indexOf(Math.min(...numbers));
+  return [...numbers.slice(0, indexOfMin), ...numbers.slice(indexOfMin + 1)];
 }
